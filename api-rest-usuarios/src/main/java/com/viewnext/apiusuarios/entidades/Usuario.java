9aa1611e0fd34e0f3a75d1e2eca8828a8b7526ab
Javier Sanchez Morales <a@a.com>
2019-12-16 15:55:40 +0100

    Usuarios - Añadidos restricciones NULL a campos en BBDD

--- api-rest-usuarios/src/main/java/com/viewnext/apiusuarios/entidades/Usuario.java
+++ api-rest-usuarios/src/main/java/com/viewnext/apiusuarios/entidades/Usuario.java
@@ -1,11 +1,19 @@
 package com.viewnext.apiusuarios.entidades;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 
 @Entity
 public class Usuario implements Serializable {
@@ -31,9 +39,18 @@ public class Usuario implements Serializable {
 	@Id
 	@GeneratedValue (strategy= GenerationType.IDENTITY)
 	private Integer id;
+	@NotNull
+	@Size(min=1,max=50)
 	private String nombre;
+	@NotNull
+	@Size(min=1,max=255)
+	@Pattern(regexp="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")
 	private String email;
+	@Size(min=1,max=50)
 	private String password;
+	@Column(name="timestamp",nullable=false,updatable=false,insertable=false,columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date timeStamp;
 	
 	public Integer getId() {
 		return id;
